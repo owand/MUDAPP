cdf6e65ac24c3a02b8b2ecc486e93511c7185e0e
Andrey <plowand@gmail.com>
2021-11-07 22:16:46 +0200

    MainPage to OnStart()

--- MUDAPP/MUDAPP/App.xaml.cs
+++ MUDAPP/MUDAPP/App.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Globalization;
 using System.IO;
 using System.Reflection;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -99,17 +100,17 @@ namespace MUDAPP
                 Device.BeginInvokeOnMainThread(async () => { await Settings.ProVersionCheck(); });
             }
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
         }
 
-        protected override void OnStart()
+        protected async override void OnStart()
         {
             // Handle when your app starts
             try
             {
                 if (!File.Exists(Constants.DatabasePath))
                 {
-                    CopyDBifNotExists();
+                    await CopyDBifNotExists();
                 }
                 else if (GetCurrentDBVersion() < Constants.dbVersion)
                 {
@@ -118,13 +119,15 @@ namespace MUDAPP
                     GC.Collect();
                     GC.WaitForPendingFinalizers();
 
-                    CopyDBifNotExists();
-                    Application.Current.MainPage.DisplayAlert("Congratulations! ", " The database has been updated!", AppResource.messageOk); // Что-то пошло не так
+                    await CopyDBifNotExists();
+                    await Application.Current.MainPage.DisplayAlert("Congratulations! ", " The database has been updated!", AppResource.messageOk); // Что-то пошло не так
                 }
+
+                MainPage = new AppShell();
             }
             catch (Exception ex)
             {
-                Application.Current.MainPage.DisplayAlert(AppResource.messageError, ex.Message, AppResource.messageOk); // Что-то пошло не так
+                await Application.Current.MainPage.DisplayAlert(AppResource.messageError, ex.Message, AppResource.messageOk); // Что-то пошло не так
             }
         }
 
@@ -139,14 +142,14 @@ namespace MUDAPP
         }
 
 
-        public void CopyDBifNotExists()
+        public async Task CopyDBifNotExists()
         {
             try
             {
                 Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"{GetType().Namespace}.{Constants.dbName}");
                 if (stream == null)
                 {
-                    Current.MainPage.DisplayAlert(AppResource.messageError, "The resource " + Constants.dbName + " was not loaded properly.", AppResource.messageOk); // Что-то пошло не так
+                    await Current.MainPage.DisplayAlert(AppResource.messageError, "The resource " + Constants.dbName + " was not loaded properly.", AppResource.messageOk); // Что-то пошло не так
                     System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
                     return;
                 }
@@ -181,7 +184,7 @@ namespace MUDAPP
             }
             catch (Exception ex)
             {
-                Application.Current.MainPage.DisplayAlert(AppResource.messageError, ex.Message, AppResource.messageOk); // Что-то пошло не так
+                await Application.Current.MainPage.DisplayAlert(AppResource.messageError, ex.Message, AppResource.messageOk); // Что-то пошло не так
                 return;
             }
         }
